c0356ff689096f7f9c67fee7d91d693e9de5b9bf
Robert Armenski <r.armenski@gmail.com>
2015-02-02 00:42:14 +0100

    Added proper URL routing

--- web2py/routes.py
+++ web2py/routes.py
@@ -1,14 +1,16 @@
 routes_in = (
-    ('/', '/MiniTimeMK/static/index.html'),
-    ('/(?P<any>index|crna_hronika|ekonomija|kultura|makedonija|skopje|sport|svet|tehnologija|zabava|zivot)',
-     '/MiniTimeMK/static/\g<any>.html'),
+    ('/', '/MiniTimeMK/default/index'),
+    ('/index', '/MiniTimeMK/default/index'),
     ('/update', '/MiniTimeMK/default/update'),
-    # ('/.*$', '/MiniTimeMK/static/index.html')   # all other urls redirect to index
+    ('/index_static', '/MiniTimeMK/static/index.html'),
+    ('/(?P<any>crna_hronika|ekonomija|kultura|makedonija|skopje|sport|svet|tehnologija|zabava|zivot)',
+     '/MiniTimeMK/static/\g<any>.html'),
 )
 routes_out = (
-    ('/MiniTimeMK/static/index.html', '/'),
-    ('/MiniTimeMK/static/(?P<any>index|crna_hronika|ekonomija|kultura|makedonija|skopje|sport|svet|tehnologija|zabava|zivot).html',
-     '/\g<any>'),
+    ('/MiniTimeMK/default/index', '/'),
+    ('/MiniTimeMK/default/index', '/index'),
     ('/MiniTimeMK/default/update', '/update'),
-    # ('/MiniTimeMK/static/index.html', '/.*$'),
+    ('/MiniTimeMK/static/index.html', '/index_static'),
+    ('/MiniTimeMK/static/(?P<any>crna_hronika|ekonomija|kultura|makedonija|skopje|sport|svet|tehnologija|zabava|zivot).html',
+     '/\g<any>'),
 )
